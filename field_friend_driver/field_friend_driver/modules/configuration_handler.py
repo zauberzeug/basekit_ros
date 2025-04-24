e0a7531882ba1e3c4911642677c8f9ffbb7bd649
Jens Ogorek <Jens.ogorek@gmail.com>
2025-04-24 16:14:08 +0200

    updated lizard configurator

--- field_friend_driver/field_friend_driver/modules/configuration_handler.py
+++ field_friend_driver/field_friend_driver/modules/configuration_handler.py
@@ -1,6 +1,8 @@
 """ Copyright (c) 2024 Leibniz-Institut für Agrartechnik und Bioökonomie e.V. (ATB)
 """
 
+import os
+
 from rclpy.node import Node
 from std_msgs.msg import String
 
@@ -10,7 +12,7 @@ from field_friend_driver.communication.communication import Communication
 class ConfigurationHandler:
     """Handle lizard configuration file."""
 
-    def __init__(self, node: Node, comm: Communication, filename: str):
+    def __init__(self, node: Node, comm: Communication, filename: str = None):
         self._node = node
         self._logger = node.get_logger()
         self._comm = comm
@@ -21,10 +23,23 @@ class ConfigurationHandler:
         )
 
     def handle_configure(self, msg: String):
-        """Push startup.liz to microcontroller."""
-        with open(self._filename) as f:
-            self._comm.send('!-')
-            for line in f.read().splitlines():
-                self._comm.send(f'!+{line}')
-            self._comm.send('!.')
-            self._comm.send('core.restart()')
+        """Push startup.liz to microcontroller if available."""
+        if not self._filename:
+            self._logger.warn('No startup file configured. Skipping configuration.')
+            return
+
+        if not os.path.exists(self._filename):
+            self._logger.error(f'Startup file {self._filename} not found!')
+            return
+
+        try:
+            with open(self._filename) as f:
+                self._logger.info(f'Applying configuration from {self._filename}')
+                self._comm.send('!-')
+                for line in f.read().splitlines():
+                    self._comm.send(f'!+{line}')
+                self._comm.send('!.')
+                self._comm.send('core.restart()')
+                self._logger.info('Configuration applied successfully')
+        except Exception as e:
+            self._logger.error(f'Failed to apply configuration: {str(e)}')
