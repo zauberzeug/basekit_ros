d3256bc8807d4b03e977bde36b211fd0605e026b
Jens Ogorek <Jens.ogorek@gmail.com>
2025-04-28 15:03:19 +0200

    added example ui

---
+++ exampleui/example_ui/__init__.py
@@ -0,0 +1,3 @@
+"""Example UI package."""
+
+__version__ = '0.0.1'
---
+++ exampleui/launch/ui.launch.py
@@ -0,0 +1,14 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='example_ui',
+            executable='ui_node.py',
+            name='ui_node',
+            output='screen',
+            prefix=['python3']
+        )
+    ])
