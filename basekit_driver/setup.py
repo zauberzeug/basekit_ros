03dd38073d9a962914dd0bdb2a125425cc62bdbc
Jens Ogorek <Jens.ogorek@gmail.com>
2025-05-09 14:51:03 +0200

    updated package files

--- basekit_driver/setup.py
+++ basekit_driver/setup.py
@@ -7,18 +7,18 @@ package_name = 'basekit_driver'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
          ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'geometry_msgs', 'sensor_msgs'],
     zip_safe=True,
     maintainer='ros',
-    maintainer_email='robotik@zauberzeug.com',
-    description='This is a driver for running the basekit hardware.',
+    maintainer_email='ros@zauberzeug.com',
+    description='This is a driver for running the a basekit robot.',
     license='MIT License',
     tests_require=['pytest'],
     entry_points={
